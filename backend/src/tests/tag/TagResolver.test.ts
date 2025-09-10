3e0ee2454be16f2a0f8775d0d33a8e04f7018afa
Emilia <Emilia@prodeadstrateur.home>
2025-09-10 14:19:29 +0200

    WIP : DP-61: tests : HelloResolver ok et test sur TagResolver  en cours

---
+++ backend/src/tests/tag/TagResolver.test.ts
@@ -0,0 +1,12 @@
+import { TagResolver } from "../../resolvers/TagResolver";
+
+// describe('TagResolver', () => {
+//     let resolver: HelloResolver;
+  
+//     resolver = new HelloResolver();
+  
+//     it("should return 'Hello world!'", () => {
+//       const result = resolver.hello();
+//       expect(result).toBe('Hello world!');
+//     });
+//   });
